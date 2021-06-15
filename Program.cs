09d2c3490c06b5fc5e873a7ac20f03e10a5c1bea
mrmay <mrmay@DESKTOP-2UCOR7B>
2021-06-15 21:16:39 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select Languages 1 ToEnglish 2 ToMorse 3 ToRussian 4 FormRussianToMorse");
+            var i = int.Parse(Console.ReadLine() ?? string.Empty);
+            Morse.Translate(i);
+        }
+    }
+}
